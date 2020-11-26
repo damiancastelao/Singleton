74f6ea537055310695e0c241f2224305f57421cb
Damian Nogueiras <damiannogueiras@gmail.com>
2020-11-26 19:19:40 +0100

    Inicio

---
+++ src/dam1/danielcastelao/org/Main.java
@@ -0,0 +1,8 @@
+package dam1.danielcastelao.org;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Proyecto para aprender a hacer un Singleton");
+    }
+}
