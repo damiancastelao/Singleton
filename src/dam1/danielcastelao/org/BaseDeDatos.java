7fd926108fafa6cace8f429cc8fa9441a3439e81
Damian Nogueiras <damian@danielcastelao.org>
2022-11-30 14:28:31 +0100

    Refactor de nombres

--- src/dam1/danielcastelao/org/BaseDeDatos.java
+++ src/dam1/danielcastelao/org/BaseDeDatos.java
@@ -3,18 +3,18 @@ package dam1.danielcastelao.org;
 /**
  * Solo una instancia
  */
-public class Singleton {
+public class BaseDeDatos {
     // atributos para comprobar que siempre es el mismo objeto
     private String nombre;
-    private Integer edad;
+    private String token;
 
     // esta va a ser nuestra unica instancia
-    private static Singleton instance = null;
+    private static BaseDeDatos instance = null;
 
     /**
      * Constructor bloqueado con private
      */
-    private Singleton() {
+    private BaseDeDatos() {
     }
 
     /**
@@ -22,13 +22,13 @@ public class Singleton {
      *
      * @return la instancia. Si no existe la crea primero
      */
-    public static Singleton getInstance() {
+    public static BaseDeDatos getInstance() {
         // compruebo si ya está creada
         if (instance == null) {
             // como no está creada, la creo
             // desde aqui si que puedo acceder al constructor
             // porque estoy en la misma clase
-            instance = new Singleton();
+            instance = new BaseDeDatos();
         }
         // devuelvo la instancia
         return instance;
@@ -36,10 +36,10 @@ public class Singleton {
 
     /**
      * Edad de la persona
-     * @param edad de la persona
+     * @param token de la persona
      */
-    public void setEdad(Integer edad) {
-        this.edad = edad;
+    public void setToken(String token) {
+        this.token = token;
     }
 
     /**
--- src/dam1/danielcastelao/org/Main.java
+++ src/dam1/danielcastelao/org/Main.java
@@ -5,28 +5,27 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Proyecto para aprender a hacer un Singleton");
 
-        //intento instanciar un objeto con el constructor privado
+        // intento instanciar un objeto con el constructor privado
         // esto es un error
         // Singleton miUnicaInstancia = new Singleton();
 
         // creamos un objeto
-        Singleton miUnicaInstancia = Singleton.getInstance();
+        BaseDeDatos miUnicaInstancia = BaseDeDatos.getInstance();
         // rellenamos los atributos
-        miUnicaInstancia.setEdad(20);
+        miUnicaInstancia.setToken("AAR3");
         miUnicaInstancia.setNombre("Pepe");
 
         // creamos otro objeto Singleton
         // como la única manera es con getInstance(), este método se encargará
         // de devolvernos el objeto creado anteriormente
-        Singleton otraInstanciaSeraLaMisma = Singleton.getInstance();
+        BaseDeDatos otraInstancia = BaseDeDatos.getInstance();
         // podemos cambiar la edad
         // comprobar con el debug que los dos objetos:
         // miUnicaInstancia y otraInstanciaSeraLaMisma son el mismo
-        otraInstanciaSeraLaMisma.setEdad(34);
-
+        otraInstancia.setToken("BG4H");
 
         // version 1.1 del programa con otra instancia
-        Singleton versionInstancia = Singleton.getInstance();
-        versionInstancia.setNombre("version 1.1");
+        BaseDeDatos versionInstancia = BaseDeDatos.getInstance();
+        versionInstancia.setNombre("version 1.2");
     }
 }
